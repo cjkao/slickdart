5dda52abaa1b693513d5b3167f79bce91aa3c39d
peter001 <kaopeter@gmail.com>
2013-10-21 00:37:44 +0800

    move to library

---
+++ lib/slick.core.dart
@@ -0,0 +1,437 @@
+library slick.core;
+import 'dart:html';
+import 'dart:convert';
+import 'dart:math' as math;
+
+EditorLock GlobalEditorLock = new EditorLock();
+
+class EventData {
+  bool _isPropagationStopped = false;
+  bool _isImmediatePropagationStopped = false;
+  String toString(){
+    return 'evd pg:' + (_isPropagationStopped ? 'T' : 'F' )+ ' imStp ' + ( _isImmediatePropagationStopped ? 'T' : 'F');
+  }
+  /***
+   * Stops event from propagating up the DOM tree.
+   * @method stopPropagation
+   */
+  stopPropagation(){
+    _isPropagationStopped = true;
+  }
+
+  /***
+   * Returns whether stopPropagation was called on this event object.
+   * @method isPropagationStopped
+   * @return {Boolean}
+   */
+  isPropagationStopped(){
+    return _isPropagationStopped;
+  }
+
+  /***
+   * Prevents the rest of the handlers from being executed.
+   * @method stopImmediatePropagation
+   */
+  stopImmediatePropagation(){
+    _isImmediatePropagationStopped = true;
+  }
+
+  /***
+   * Returns whether stopImmediatePropagation was called on this event object.\
+   * @method isImmediatePropagationStopped
+   * @return {Boolean}
+   */
+  isImmediatePropagationStopped(){
+    return _isImmediatePropagationStopped;
+  }
+
+}
+
+
+class Event {
+  List<Function> handlers = [];
+
+  /***
+   * Adds an event handler to be called when the event is fired.
+   * <p>Event handler will receive two arguments - an <code>EventData</code> and the <code>data</code>
+   * object the event was fired with.<p>
+   * @method subscribe
+   * @param fn {Function} Event handler.
+   */
+  subscribe (Function fn) {
+    handlers.add(fn);
+  }
+
+  /***
+   * Removes an event handler added with <code>subscribe(fn)</code>.
+   * @method unsubscribe
+   * @param fn {Function} Event handler to be removed.
+   */
+  bool unsubscribe (Function fn) => handlers.remove(fn);
+//  {
+//    for (var i = handlers.length - 1; i >= 0; i--) {
+//      if (handlers[i] == fn) {
+//        handlers.splice(i, 1);
+//      }
+//    }
+//  };
+
+  /***
+   * Fires an event notifying all subscribers.
+   * @method notify
+   * @param args {Object} Additional data object to be passed to all handlers.
+   * @param e {EventData}
+   *      Optional.
+   *      An <code>EventData</code> object to be passed to all handlers.
+   *      For DOM events, an existing W3C/jQuery event object can be passed in.
+   * @param scope {Object}
+   *      Optional.
+   *      The scope ("this") within which the handler will be executed.
+   *      If not specified, the scope will be set to the <code>Event</code> instance.
+   */
+  notify(args, [dynamic e, scope]) {
+    if(e==null) e = new EventData();
+   // scope = scope || this;
+    var returnValue;
+    print('evt:' + e.toString() + ' arg:' + args.toString());
+    for (int i = 0; i < handlers.length && !(e is EventData && (e.isPropagationStopped() || e.isImmediatePropagationStopped())); i++) {
+      print('hand:' + handlers[i].toString());
+      returnValue = Function.apply(handlers[i],[e,args]);
+    }
+
+    return returnValue;
+  }
+}
+
+class EventHandler{
+  List<Map<String,dynamic>> handlers = [];
+
+  subscribe  (Event event, Function handler) {
+    handlers.add({
+      'event': event,
+      'handler': handler
+    });
+    event.subscribe(handler);
+
+    return this;  // allow chaining
+  }
+
+  unsubscribe  (Event event,Function handler) {
+    var i = handlers.length;
+    while (i-- > 0) {
+      if (handlers[i]['event'] == event &&
+          handlers[i]['handler'] == handler) {
+        handlers.removeAt(i);
+        event.unsubscribe(handler);
+        return;
+      }
+    }
+
+    return this;  // allow chaining
+  }
+
+  unsubscribeAll (){
+    var i = handlers.length;
+    while (i-- > 0) {
+      handlers[i]['event'].unsubscribe(handlers[i]['handler']);
+    }
+    handlers = [];
+
+    return this;  // allow chaining
+  }
+}
+
+
+
+/***
+ * A structure containing a range of cells.
+ * @class Range
+ * @constructor
+ * @param fromRow {Integer} Starting row.
+ * @param fromCell {Integer} Starting cell.
+ * @param toRow {Integer} Optional. Ending row. Defaults to <code>fromRow</code>.
+ * @param toCell {Integer} Optional. Ending cell. Defaults to <code>fromCell</code>.
+ */
+class Range{
+  int fromRow,fromCell, toRow,toCell;
+  Range(this.fromRow, this.fromCell,[ this.toRow, this.toCell]) {
+    if (toRow == null && toCell == null) {
+      toRow = fromRow;
+      toCell = fromCell;
+    }
+
+    /***
+     * @property fromRow
+     * @type {Integer}
+     */
+    fromRow = math.min(fromRow, toRow);
+
+    /***
+     * @property fromCell
+     * @type {Integer}
+     */
+    this.fromCell = math.min(fromCell, toCell);
+
+    /***
+     * @property toRow
+     * @type {Integer}
+     */
+    this.toRow = math.max(fromRow, toRow);
+
+    /***
+     * @property toCell
+     * @type {Integer}
+     */
+    this.toCell = math.max(fromCell, toCell);
+  }
+
+
+  /***
+   * Returns whether a range represents a single row.
+   * @method isSingleRow
+   * @return {Boolean}
+   */
+  bool isSingleRow () {
+    return this.fromRow == this.toRow;
+  }
+
+  /***
+   * Returns whether a range represents a single cell.
+   * @method isSingleCell
+   * @return {Boolean}
+   */
+  bool isSingleCell() {
+    return this.fromRow == this.toRow && this.fromCell == this.toCell;
+  }
+
+  /***
+   * Returns whether a range contains a given cell.
+   * @method contains
+   * @param row {Integer}
+   * @param cell {Integer}
+   * @return {Boolean}
+   */
+  bool contains (int row, int cell) {
+    return row >= this.fromRow && row <= this.toRow &&
+        cell >= this.fromCell && cell <= this.toCell;
+  }
+
+  /***
+   * Returns a readable representation of a range.
+   * @method toString
+   * @return {String}
+   */
+  String toString () {
+    if (this.isSingleCell()) {
+      return "( + $fromRow : $fromCell )";
+    }
+    else {
+      return "( $fromRow : $fromCell - $toRow : $toCell )";
+    }
+  }
+}
+
+
+/***
+ * A base class that all special / non-data rows (like Group and GroupTotals) derive from.
+ * @class NonDataItem
+ * @constructor
+ */
+class NonDataItem {
+  bool _nonDataRow = true;
+  bool get nonDataRow => _nonDataRow;
+}
+
+
+/***
+ * Information about a group of rows.
+ * @class Group
+ * @extends Slick.NonDataItem
+ * @constructor
+ */
+class Group extends NonDataItem {
+  bool __group = true;
+
+  /**
+   * Grouping level, starting with 0.
+   * @property level
+   * @type {Number}
+   */
+  int level = 0;
+
+  /***
+   * Number of rows in the group.
+   * @property count
+   * @type {Integer}
+   */
+  int count = 0;
+
+  /***
+   * Grouping value.
+   * @property value
+   * @type {Object}
+   */
+  Object value = null;
+
+  /***
+   * Formatted display value of the group.
+   * @property title
+   * @type {String}
+   */
+  String title = null;
+
+  /***
+   * Whether a group is collapsed.
+   * @property collapsed
+   * @type {Boolean}
+   */
+  bool collapsed = false;
+
+  /***
+   * GroupTotals, if any.
+   * @property totals
+   * @type {GroupTotals}
+   */
+  GroupTotals totals = null;
+
+  /**
+   * Rows that are part of the group.
+   * @property rows
+   * @type {Array}
+   */
+  List rows = [];
+
+  /**
+   * Sub-groups that are part of the group.
+   * @property groups
+   * @type {Array}
+   */
+  List groups = null;
+
+  /**
+   * A unique key used to identify the group.  This key can be used in calls to DataView
+   * collapseGroup() or expandGroup().
+   * @property groupingKey
+   * @type {Object}
+   */
+  Object groupingKey = null;
+
+  /***
+   * Compares two Group instances.
+   * @method equals
+   * @return {Boolean}
+   * @param group {Group} Group instance to compare to.
+   */
+  operator==(Group group){
+    return this.value == group.value &&
+        this.count == group.count &&
+        this.collapsed == group.collapsed &&
+        this.title == group.title;
+  }
+
+}
+
+
+
+
+/***
+ * Information about group totals.
+ * An instance of GroupTotals will be created for each totals row and passed to the aggregators
+ * so that they can store arbitrary data in it.  That data can later be accessed by group totals
+ * formatters during the display.
+ * @class GroupTotals
+ * @extends Slick.NonDataItem
+ * @constructor
+ */
+class GroupTotals extends NonDataItem{
+  bool __groupTotals = true;
+
+  /***
+   * Parent Group.
+   * @param group
+   * @type {Group}
+   */
+  Group group = null;
+}
+
+
+/***
+ * A locking helper to track the active edit controller and ensure that only a single controller
+ * can be active at a time.  This prevents a whole class of state and validation synchronization
+ * issues.  An edit controller (such as SlickGrid) can query if an active edit is in progress
+ * and attempt a commit or cancel before proceeding.
+ * @class EditorLock
+ * @constructor
+ */
+class EditorLock {
+  var activeEditController = null;
+
+  /***
+   * Returns true if a specified edit controller is active (has the edit lock).
+   * If the parameter is not specified, returns true if any edit controller is active.
+   * @method isActive
+   * @param editController {EditController}
+   * @return {Boolean}
+   */
+  bool isActive([editController]) {
+    return (editController!=null ? activeEditController == editController : activeEditController != null);
+  }
+
+  /***
+   * Sets the specified edit controller as the active edit controller (acquire edit lock).
+   * If another edit controller is already active, and exception will be thrown.
+   * @method activate
+   * @param editController {EditController} edit controller acquiring the lock
+   */
+  void activate(editController) {
+    if (editController == activeEditController) { // already activated?
+      return;
+    }
+    if (activeEditController != null) {
+      throw "SlickGrid.EditorLock.activate: an editController is still active, can't activate another editController";
+    }
+    if (editController['commitCurrentEdit']==null) {
+      throw "SlickGrid.EditorLock.activate: editController must implement .commitCurrentEdit()";
+    }
+    if (editController['cancelCurrentEdit']==null) {
+      throw "SlickGrid.EditorLock.activate: editController must implement .cancelCurrentEdit()";
+    }
+    activeEditController = editController;
+  }
+
+  /***
+   * Unsets the specified edit controller as the active edit controller (release edit lock).
+   * If the specified edit controller is not the active one, an exception will be thrown.
+   * @method deactivate
+   * @param editController {EditController} edit controller releasing the lock
+   */
+  void deactivate (editController) {
+    if (activeEditController != editController) {
+      throw "SlickGrid.EditorLock.deactivate: specified editController is not the currently active one";
+    }
+    activeEditController = null;
+  }
+
+  /***
+   * Attempts to commit the current edit by calling "commitCurrentEdit" method on the active edit
+   * controller and returns whether the commit attempt was successful (commit may fail due to validation
+   * errors, etc.).  Edit controller's "commitCurrentEdit" must return true if the commit has succeeded
+   * and false otherwise.  If no edit controller is active, returns true.
+   * @method commitCurrentEdit
+   * @return {Boolean}
+   */
+  bool commitCurrentEdit () {
+    return (activeEditController!=null ? activeEditController['commitCurrentEdit']() : true);
+  }
+
+  /***
+   * Attempts to cancel the current edit by calling "cancelCurrentEdit" method on the active edit
+   * controller and returns whether the edit was successfully cancelled.  If no edit controller is
+   * active, returns true.
+   * @method cancelCurrentEdit
+   * @return {Boolean}
+   */
+  bool cancelCurrentEdit() {
+    return (activeEditController!=null ? activeEditController['cancelCurrentEdit']() : true);
+  }
+}
---
+++ lib/slick.selectionmodel.dart
@@ -0,0 +1,297 @@
+library slick.selection;
+import 'dart:html';
+import 'slick.core.dart';
+import 'slick.grid.dart';
+import 'dart:collection';
+class CellRangeDecorator{
+  Element _elem;
+  Map options;
+  SlickGrid _grid;
+  Map _defaults = {
+                   'selectionCssClass': 'slick-range-decorator',
+                   'selectionCss': {
+                     "zIndex": "9999",
+                     "border": "2px dashed red"
+                   }
+  };
+  CellRangeDecorator(SlickGrid sg){
+    _grid=sg;
+    options = new Map.from(_defaults);
+    options.addAll(sg.options);
+  }
+  Element show(Range range) {
+    if (!_elem) {
+      _elem = new DivElement();
+      _elem.style.zIndex= options['selectionCss']['zIndex'];
+      _elem.style.zIndex= options['selectionCss']['border'];
+      _elem.classes.add(options['selectionCssClass']);
+      _grid.getCanvasNode().children.add(_elem);
+    }
+      Map from = _grid.getCellNodeBox(range.fromRow, range.fromCell);
+      var to = _grid.getCellNodeBox(range.toRow, range.toCell);
+      _elem.style..top=  from['top'] - 1
+                 ..left =from['left'] - 1
+                 ..height=  to['bottom'] - from['top'] - 2
+                 ..width= to['right'] - from['left'] - 2;
+      return _elem;
+    }
+
+  hide() {
+    if (_elem!=null) {
+      _elem.remove();
+      _elem = null;
+    }
+  }
+}
+
+
+class CellRangeSelector{
+  Event onBeforeCellRangeSelected = new Event();
+  Event onCellRangeSelected = new Event();
+  CellRangeSelector(options){
+
+  }
+  Map options;
+  SlickGrid _grid;
+  Element _canvas;
+  var _dragging;
+  var _decorator;
+//  v/ar _self = this;
+  var _handler = new EventHandler();
+  var _defaults = {
+                   'selectionCss': {
+                     "border": "2px dashed blue"
+                   }
+  };
+
+  init(SlickGrid grid) {
+    options = new Map.from(_defaults);
+    options.addAll(grid.options);
+    _decorator = new CellRangeDecorator(grid);
+    _grid = grid;
+    _canvas = _grid.getCanvasNode();
+    _handler
+    .subscribe(_grid.onDragInit, handleDragInit)
+      .subscribe(_grid.onDragStart, handleDragStart)
+        .subscribe(_grid.onDrag, handleDrag)
+        .subscribe(_grid.onDragEnd, handleDragEnd);
+    }
+
+    destroy() {
+      _handler.unsubscribeAll();
+    }
+    handleDragInit(e, dd) {
+      // prevent the grid from cancelling drag'n'drop by default
+      e.stopImmediatePropagation();
+    }
+    handleDragStart(e, dd) {
+      Map cell = _grid.getCellFromEvent(e);
+      if (onBeforeCellRangeSelected.notify(cell) != false) {
+        if (_grid.canCellBeSelected(cell['row'], cell['cell'])) {
+          _dragging = true;
+          e.stopImmediatePropagation();
+        }
+      }
+      if (!_dragging) {
+        return;
+      }
+
+      _grid.setFocus(); //no _grid.focus, may from drag plugin
+
+      var start = _grid.getCellFromPoint(
+          dd.startX - _canvas.offset.left,
+          dd.startY - _canvas.offset.top);
+
+      dd.range = {'start': start, 'end': {}};
+
+      return _decorator.show(new Range(start['row'], start['cell']));
+    }
+
+    handleDrag(e, dd) {
+      if (!_dragging) {
+        return;
+      }
+      e.stopImmediatePropagation();
+
+      var end = _grid.getCellFromPoint(
+          e.pageX - _canvas.offset.left,
+          e.pageY - _canvas.offset.top);
+
+      if (!_grid.canCellBeSelected(end['row'], end['cell'])) {
+        return;
+      }
+
+      dd.range.end = end;
+      _decorator.show(new Range(dd.range.start.row, dd.range.start.cell, end['row'], end['cell']));
+    }
+    handleDragEnd(e, dd) {
+      if (!_dragging) {
+        return;
+      }
+
+      _dragging = false;
+      e.stopImmediatePropagation();
+
+      _decorator.hide();
+      onCellRangeSelected.notify({
+        'range': new Range(
+            dd.range.start.row,
+            dd.range.start.cell,
+            dd.range.end.row,
+            dd.range.end.cell
+        )
+      });
+    }
+
+}
+abstract class SelectionModel{
+  init(SlickGrid grid);
+  destroy();
+  void setSelectedRanges(List ranges);
+  List getSelectedRanges();
+}
+class CellSelectionModel extends SelectionModel{
+   Event onSelectedRangesChanged = new Event();
+
+     CellSelectionModel(Map options){
+       _options = new Map.from(options);
+       _options['selectActiveCell']=true;
+
+     }
+    SlickGrid _grid;
+    Element _canvas;
+    List _ranges = [];
+    var _selector = new CellRangeSelector({
+      "selectionCss": {
+        "border": "2px solid black"
+      }
+    });
+    var _options;
+    var _defaults = {  'selectActiveCell': true };
+
+
+    init(SlickGrid grid) {
+
+      _grid = grid;
+      _canvas = _grid.getCanvasNode();
+      _grid.onActiveCellChanged.subscribe(_handleActiveCellChange);
+      _grid.onKeyDown.subscribe(_handleKeyDown);
+      grid.registerPlugin(_selector);
+      _selector.onCellRangeSelected.subscribe(_handleCellRangeSelected);
+      _selector.onBeforeCellRangeSelected.subscribe(_handleBeforeCellRangeSelected);
+    }
+
+    destroy() {
+      _grid.onActiveCellChanged.unsubscribe(_handleActiveCellChange);
+      _grid.onKeyDown.unsubscribe(_handleKeyDown);
+      _selector.onCellRangeSelected.unsubscribe(_handleCellRangeSelected);
+      _selector.onBeforeCellRangeSelected.unsubscribe(_handleBeforeCellRangeSelected);
+      _grid.unregisterPlugin(_selector);
+    }
+
+    List _removeInvalidRanges(List ranges) {
+      var result = [];
+
+      for (var i = 0; i < ranges.length; i++) {
+        var r = ranges[i];
+        if (_grid.canCellBeSelected(r.fromRow, r.fromCell) && _grid.canCellBeSelected(r.toRow, r.toCell)) {
+          result.add(r);
+        }
+      }
+
+      return result;
+    }
+
+    void setSelectedRanges(List ranges) {
+      _ranges = _removeInvalidRanges(ranges);
+      onSelectedRangesChanged.notify(_ranges);
+    }
+
+    List getSelectedRanges() {
+      return _ranges;
+    }
+
+    _handleBeforeCellRangeSelected(e, args) {
+      if (_grid.getEditorLock().isActive()) {
+        e.stopPropagation();
+        return false;
+      }
+    }
+
+    _handleCellRangeSelected(e, args) {
+      setSelectedRanges(args.range);
+    }
+  /**
+   * args: object
+        cell: 0
+        grid: SlickGrid
+       row: 6
+   */
+    _handleActiveCellChange(EventData e, Map<String,dynamic>args) {
+      if (_options['selectActiveCell'] && args['row'] != null && args['cell'] != null) {
+         setSelectedRanges([new Range(args['row'], args['cell'])]);
+      }
+    }
+
+    _handleKeyDown(KeyboardEvent e,[args]) {
+      /***
+       * byte codes
+       * 37 left
+       * 38 up
+       * 39 right
+       * 40 down
+       */
+      List ranges;
+      var last;
+      Map<String,int> active = _grid.getActiveCell();
+
+      if ( active !=null && e.shiftKey && !e.ctrlKey && !e.altKey &&
+          (e.which == 37 || e.which == 39 || e.which == 38 || e.which == 40) ) {
+
+        ranges = getSelectedRanges();
+        if (ranges.length>0)
+         ranges.add(new Range(active['row'], active['cell']));
+
+        // keyboard can work with last range only
+        last = ranges.removeLast();
+
+        // can't handle selection out of active cell
+        if (!last.contains(active['row'], active['cell']))
+          last = new Range(active['row'], active['cell']);
+
+        var dRow = last.toRow - last.fromRow,
+            dCell = last.toCell - last.fromCell,
+            // walking direction
+            dirRow = active['row'] == last.fromRow ? 1 : -1,
+            dirCell = active['cell'] == last.fromCell ? 1 : -1;
+
+        if (e.which == 37) {
+          dCell -= dirCell;
+        } else if (e.which == 39) {
+          dCell += dirCell ;
+        } else if (e.which == 38) {
+          dRow -= dirRow;
+        } else if (e.which == 40) {
+          dRow += dirRow;
+        }
+
+        // define new selection range
+        var new_last = new Range(active['row'], active['cell'], active['row'] + dirRow*dRow, active['cell'] + dirCell*dCell);
+        if (_removeInvalidRanges([new_last]).length>0) {
+          ranges.add(new_last);
+          int viewRow = dirRow > 0 ? new_last.toRow : new_last.fromRow;
+          int viewCell = dirCell > 0 ? new_last.toCell : new_last.fromCell;
+         _grid.scrollRowIntoView(viewRow,false);
+         _grid.scrollCellIntoView(viewRow, viewCell,false);
+        }
+        else
+          ranges.add(last);
+
+        setSelectedRanges(ranges);
+
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    }
+   }
+
