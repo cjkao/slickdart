3e675dd6aba959d1cb6b89b3278df17ad53b39b4
peter001 <kaopeter@gmail.com>
2015-02-02 22:38:38 +0800

    add example of selectList

--- web/example/EditorSample.dart
+++ web/example/EditorSample.dart
@@ -1,50 +1,43 @@
 import 'dart:html';
 import 'package:slickdart/slick.dart' as grid;
 import 'dart:math' as math;
-//import 'package:slickdart/slick_editor.dart';
+import 'package:slickdart/slick_editor.dart';
 import 'package:slickdart/slick_selectionmodel.dart';
 void main() {
   grid.SlickGrid  g=init();
   g.init();
-  //print (g.$headerScroller.querySelectorAll('.slick-header-column').length);
-  querySelector('#reset').onClick.listen((e){
-    List _data=[];
-    for (var i = 0; i < 50000; i++) {
-      _data.add( {
-        'dtitle':  new math.Random().nextInt(1000).toString(),
-        'duration': new math.Random().nextInt(1000).toString(),
-        'pc': i
-      });
-    }
-    g.data.clear();
-    g.data.addAll(_data);
-    g.invalidate();
-    g.render();
-
-    //print(g.data);
-  });
-//  query('#reset').onClick.listen((e){
-//    print(g.data);
+//  querySelector('#reset').onClick.listen((e){
+//    List _data=[];
+//    for (var i = 0; i < 50000; i++) {
+//      _data.add( {
+//        'dtitle':  new math.Random().nextInt(1000).toString(),
+//        'duration': new math.Random().nextInt(1000).toString(),
+//        'pc': i
+//      });
+//    }
+//    g.data.clear();
+//    g.data.addAll(_data);
+//    g.invalidate();
+//    g.render();
+//
 //  });
 }
 
 grid.SlickGrid init(){
   Element el =querySelector('#grid');
   List column = [
-                 new grid.Column.fromMap ({'id': "title", 'name': "Title1", 'field': "dtitle", 'sortable': true,'editor': 'TextEditor' }),
-                 new grid.Column.fromMap ({'width':120,'id': "duration", 'name': "duration", 'field': "duration", 'sortable': true }),
+                 new grid.Column.fromMap ({ 'field': "dtitle", 'sortable': true,'editor': 'TextEditor' }),
+                 new grid.Column.fromMap ({'width':120, 'field': "duration", 'sortable': true }),
                  new grid.Column.fromMap ({'id': "%", 'name': "percent", 'field': "pc", 'sortable': true }),
-                // new grid.Column.fromMap ({'id': "start", 'name': "finish", 'field': "finish"})
+                 new grid.Column.fromMap ({'field': "City", 'editor': new SelectListEditor({"NY":"New York", "TPE":"Taipei"})})
                  ];
   List data=[];
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 50; i++) {
     data.add( {
       'dtitle':  new math.Random().nextInt(100).toString(),
       'duration': new math.Random().nextInt(100).toString(),
       'pc': new math.Random().nextInt(10) * 100,
-//      'start': "01/01/2009",
-//      'finish': (new math.Random().nextInt(10)+10).toString() + "/05/2013",
-//      'effortDriven': (i % 5 == 0)
+      'City': "NY"
     });
   }
   Map opt = {'explicitInitialization': false,
@@ -55,7 +48,10 @@ grid.SlickGrid init(){
 
   sg.setSelectionModel(new CellSelectionModel(sg.options));
 
-
+  sg.onBeforeEditCell.subscribe((e,args){
+    //swap editor here
+    print(args['column']);
+  });
   sg.onSort.subscribe( (e, args) {
     var cols = args['sortCols'];
 //{sortCol: {name: Title1, resizable: true, sortable: true, minWidth: 30, rerenderOnResize: false, headerCssClass: null, defaultSortAsc: true, focusable: true, selectable: true, cannotTriggerInsert: false, width: 80, id: title, field: title}, sortAsc: true}
